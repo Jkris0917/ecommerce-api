cce59581b78146dbe3da8da022338778d5aa0508
Jkris0917 <gelladojohnkris@gmail.com>
2026-06-03 21:17:10 +0800

    fix: remove duplicate DATABASES block, fix DEBUG and ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY','django-insecure-*f84+g@s@9tsjnuc90)+s6uncb-ys5dpk4t0_0mh=f$n&e&yw3')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG','True') == True
+DEBUG = os.getenv('DEBUG','True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOST','localhost 127.0.0.1').split()
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS','localhost 127.0.0.1').split()
 
 
 # Application definition
@@ -129,20 +129,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'ecommerce'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASS', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
-}
 
 
 # Password validation
