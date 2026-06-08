080329d66f885f677733f02493a014267dc1b4f5
Jkris0917 <gelladojohnkris@gmail.com>
2026-06-08 17:42:51 +0800

    fix: rewrite cors

--- config/settings.py
+++ config/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  # Next.js dev server
-    'https://benta-hive.vercel.app',
+    'https://kalakal-core.vercel.app',
 ]
 
 REST_FRAMEWORK = {
